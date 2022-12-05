eb1295cffc81ff6df1c941652384b4e8b27897b4
Brunna Serafina <brunnaserafina@gmail.com>
2022-12-05 14:15:04 -0300

    style: adding global style with reset css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
